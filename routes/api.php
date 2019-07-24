f30c636dc1398e95d2a1bcc9fd12247d91f426ba
AJay Marathe <0107amar@gmail.com>
2019-07-24 22:58:27 +0530

    authentication route has been done

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,16 @@ Route::apiResource('/post','PostController');
 Route::apiResource('/category','CategoryController');
 Route::apiResource('/post/{post}/comment','CommentController');
 
+Route::group([ 'prefix' => 'auth' ], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('signup', 'AuthController@signup');
+  
+    Route::group([ 'middleware' => 'auth:api' ], function() {
+        Route::get('logout', 'AuthController@logout');
+        Route::get('user', 'AuthController@user');
+    });
+});
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
